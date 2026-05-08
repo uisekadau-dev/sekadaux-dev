a400fa7a4f662c41c6739089ace5a4d8a6fcb1a9
uisekadau-dev <uisekadaux.dev@gmail.com>
2026-05-08 15:24:30 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
-import { BlogPosts } from 'app/components/posts'
-
-export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
-}
+<h1 className="mb-8 text-2xl font-medium tracking-tighter">
+  Sekadau Dev
+</h1>
+<p className="prose prose-neutral dark:prose-invert">
+  Ngoding dari ujung Kalimantan. Frontend enjoyer. Anak Sekadau yang mimpi bikin karya digital buat dunia. DM kalo mau collab 🚀
+</p>
