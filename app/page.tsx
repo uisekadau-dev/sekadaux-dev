45bff4d632831ca59418b311c20f351f6f769355
uisekadau-dev <uisekadaux.dev@gmail.com>
2026-05-08 15:43:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-<h1 className="mb-8 text-2xl font-medium tracking-tighter">
-  Sekadau Dev
-</h1>
-<p className="prose prose-neutral dark:prose-invert">
-  Ngoding dari ujung Kalimantan. Frontend enjoyer. Anak Sekadau yang mimpi bikin karya digital buat dunia. DM kalo mau collab 🚀
-</p>
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+        Sekadau Dev
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        Ngoding dari ujung Kalimantan. Frontend enjoyer. Anak Sekadau yang mimpi bikin karya digital buat dunia. DM kalo mau collab 🚀
+      </p>
+    </section>
+  )
+}
